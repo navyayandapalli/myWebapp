83c91b06d8ab291178bc3edb12d40d826fe4b345
navyayandapalli <navyayandapalli@gmail.com>
2025-02-10 16:30:27 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask
-
-app = Flask("MyFlaskapp")
-
+app = Flask(__name__)
 @app.route('/')
 def home():
-    return "Hello, Flask is running!"
-  app.run(host='0.0.0.0', port=5000)
+    return "Welcome to the Dockerized Flask App!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
